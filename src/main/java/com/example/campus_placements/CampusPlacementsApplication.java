26dd6a407afcf5944a94659a39d6992c20c6f0e7
Sricharan-Kasulanati <sricharankasulanati2001@gmail.com>
2025-10-14 14:53:53 -0500

    Created Backend Using springBoot

---
+++ src/main/java/com/example/campus_placements/CampusPlacementsApplication.java
@@ -0,0 +1,13 @@
+package com.example.campus_placements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPlacementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusPlacementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/campus_placements/CampusPlacementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_placements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusPlacementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
